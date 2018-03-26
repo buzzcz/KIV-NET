eaa44ea51aff9109d1671ac4812195d3187b16d8
Jaroslav Klaus <buzz3333@gmail.com>
2018-03-26 18:36:33 +0200

    Fix names in book citations Add book citation tests

--- PublicationsCore/service/CitationService.cs
+++ PublicationsCore/service/CitationService.cs
@@ -5,29 +5,63 @@ namespace PublicationsCore.Service
 {
     public class CitationService : ICitationService
     {
-        private static string GetBookCitation(BookDto publication)
+        private static string GetBookCitation(BookDto book)
         {
             string citation = "";
 
-            if (publication.AuthorPublicationList != null)
+            if (book.AuthorPublicationList != null)
             {
-                AuthorDto author = publication.AuthorPublicationList[0].Author;
-                citation += $"{author.LastName.ToUpper()}, {author.FirstName}";
+                AuthorDto author = book.AuthorPublicationList[0].Author;
+                citation = AddFirstAuthor(author, citation);
 
-                int count = publication.AuthorPublicationList.Count;
+                int count = book.AuthorPublicationList.Count;
                 for (int i = 1; i < count; i++)
                 {
-                    citation += i != count - 1 ? ", " : " a ";
-
-                    author = publication.AuthorPublicationList[i].Author;
-                    citation += $"{author.FirstName} {author.LastName.ToUpper()}";
+                    citation = AddNextAuthor(book.AuthorPublicationList[i].Author, citation, i != count - 1);
                 }
 
                 citation += ". ";
             }
 
             citation +=
-                $"{publication.Title}. {publication.Edition}. {publication.Publisher.Address}: {publication.Publisher.Name}, {publication.Date:yyyy}. ISBN {publication.Isbn}.";
+                $"{book.Title}. {book.Edition}. {book.Publisher.Address}: {book.Publisher.Name}, {book.Date:yyyy}. ISBN {book.Isbn}.";
+
+            return citation;
+        }
+
+        private static string AddNextAuthor(AuthorDto author, string citation, bool comma)
+        {
+            citation += comma ? "," : " a";
+
+            if (author.FirstName != null)
+            {
+                citation += $" {author.FirstName}";
+            }
+
+            if (author.LastName != null)
+            {
+                citation += $" {author.LastName.ToUpper()}";    
+            }
+            
+            return citation;
+        }
+
+        private static string AddFirstAuthor(AuthorDto author, string citation)
+        {
+            if (author.LastName != null)
+            {
+                citation += $"{author.LastName.ToUpper()}";
+            }
+
+            if (author.FirstName != null)
+            {
+                if (author.LastName != null)
+                {
+                    citation += ", ";
+                }
+
+                citation += $"{author.FirstName}";
+            }
 
             return citation;
         }
--- TestProject/service/CitationServiceTest.cs
+++ TestProject/service/CitationServiceTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using PublicationsCore.Facade.Dto;
 using PublicationsCore.Service;
 using TestProject.Utils;
@@ -19,14 +20,183 @@ namespace TestProject.Service
             _citationService = new CitationService();
         }
 
-        [Fact]
-        public void TestBookCitation()
+        [Theory]
+        [InlineData("Adams", null, "Hitchhiker's Guide to the Galaxy", "1st Edition", "Pilsen", "University Press",
+            "1990", "7892347-913-2341-09",
+            "ADAMS. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData(null, "Douglas", "Hitchhiker's Guide to the Galaxy", "1st Edition", "Pilsen", "University Press",
+            "1990", "7892347-913-2341-09",
+            "Douglas. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData(null, null, "Hitchhiker's Guide to the Galaxy", "1st Edition", "Pilsen", "University Press",
+            "1990", "7892347-913-2341-09",
+            "Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData("Adams", "Douglas", "Hitchhiker's Guide to the Galaxy", "1st Edition", "Pilsen", "University Press",
+            "1990", "7892347-913-2341-09",
+            "ADAMS, Douglas. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        public void Test_CorrectBookOneAuthor_CorrectBookCitation(string lastName, string name, string title,
+            string edition,
+            string address, string publisher, string date, string isbn, string expected)
         {
-            string expected =
-                $"ADAMS, Douglas. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, {DateTime.Today:yyyy}. ISBN 7892347-913-2341-09.";
-            PublicationDto publicationDto = TestUtils.CreateBook();
-            _output.WriteLine($"Getting citation for {publicationDto} in BOOK CITATION test.");
-            string citation = _citationService.GetCitation(publicationDto);
+            BookDto bookDto = new BookDto
+            {
+                Date = new DateTime(int.Parse(date), 1, 1),
+                Edition = edition,
+                Isbn = isbn,
+                Publisher = new PublisherDto
+                {
+                    Address = address,
+                    Name = publisher
+                },
+                Title = title
+            };
+
+            if (lastName != null || name != null)
+            {
+                bookDto.AuthorPublicationList = new List<AuthorPublicationDto>
+                {
+                    new AuthorPublicationDto
+                    {
+                        Author = new AuthorDto
+                        {
+                            FirstName = name,
+                            LastName = lastName
+                        }
+                    }
+                };
+            }
+
+            _output.WriteLine($"Getting citation for {bookDto} in BOOK CITATION test.");
+            string citation = _citationService.GetCitation(bookDto);
+            _output.WriteLine($"Got citation {citation} in BOOK CITATION test.");
+
+            Assert.Equal(expected, citation);
+        }
+
+        [Theory]
+        [InlineData("Adams", null, "Moffat", null, "1990",
+            "ADAMS a MOFFAT. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData(null, "Douglas", null, "Steven", "1990",
+            "Douglas a Steven. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData("Adams", "Douglas", "Moffat", null, "1990",
+            "ADAMS, Douglas a MOFFAT. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData("Adams", "Douglas", null, "Steven", "1990",
+            "ADAMS, Douglas a Steven. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData("Adams", "Douglas", "Moffat", "Steven", "1990",
+            "ADAMS, Douglas a Steven MOFFAT. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        public void Test_CorrectBookTwoAuthors_CorrectBookCitation(string lastName, string name, string lastName2,
+            string name2, string date, string expected)
+        {
+            BookDto bookDto = TestUtils.CreateBook();
+            bookDto.Date = new DateTime(int.Parse(date), 1, 1);
+
+            if (lastName != null || name != null)
+            {
+                bookDto.AuthorPublicationList = new List<AuthorPublicationDto>
+                {
+                    new AuthorPublicationDto
+                    {
+                        Author = new AuthorDto
+                        {
+                            FirstName = name,
+                            LastName = lastName
+                        }
+                    }
+                };
+            }
+
+            if (lastName2 != null || name2 != null)
+            {
+                bookDto.AuthorPublicationList.Add(new AuthorPublicationDto
+                    {
+                        Author = new AuthorDto
+                        {
+                            FirstName = name2,
+                            LastName = lastName2
+                        }
+                    }
+                );
+            }
+
+            _output.WriteLine($"Getting citation for {bookDto} in BOOK CITATION test.");
+            string citation = _citationService.GetCitation(bookDto);
+            _output.WriteLine($"Got citation {citation} in BOOK CITATION test.");
+
+            Assert.Equal(expected, citation);
+        }
+        
+        [Theory]
+        [InlineData("Adams", null, "Moffat", null, "1990",
+            "ADAMS, MOFFAT, ADAMS a MOFFAT. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData(null, "Douglas", null, "Steven", "1990",
+            "Douglas, Steven, Steven a Douglas. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData("Adams", "Douglas", "Moffat", null, "1990",
+            "ADAMS, Douglas, MOFFAT, ADAMS a Douglas MOFFAT. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData("Adams", "Douglas", null, "Steven", "1990",
+            "ADAMS, Douglas, Steven, Steven ADAMS a Douglas. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        [InlineData("Adams", "Douglas", "Moffat", "Steven", "1990",
+            "ADAMS, Douglas, Steven MOFFAT, Steven ADAMS a Douglas MOFFAT. Hitchhiker's Guide to the Galaxy. 1st Edition. Pilsen: University Press, 1990. ISBN 7892347-913-2341-09.")]
+        public void Test_CorrectBookMoreAuthors_CorrectBookCitation(string lastName, string name, string lastName2,
+            string name2, string date, string expected)
+        {
+            BookDto bookDto = TestUtils.CreateBook();
+            bookDto.Date = new DateTime(int.Parse(date), 1, 1);
+
+            if (lastName != null || name != null)
+            {
+                bookDto.AuthorPublicationList = new List<AuthorPublicationDto>
+                {
+                    new AuthorPublicationDto
+                    {
+                        Author = new AuthorDto
+                        {
+                            FirstName = name,
+                            LastName = lastName
+                        }
+                    }
+                };
+            }
+
+            if (lastName2 != null || name2 != null)
+            {
+                bookDto.AuthorPublicationList.Add(new AuthorPublicationDto
+                    {
+                        Author = new AuthorDto
+                        {
+                            FirstName = name2,
+                            LastName = lastName2
+                        }
+                    }
+                );
+            }
+            
+            if (lastName != null || name2 != null)
+            {
+                bookDto.AuthorPublicationList.Add(new AuthorPublicationDto
+                    {
+                        Author = new AuthorDto
+                        {
+                            FirstName = name2,
+                            LastName = lastName
+                        }
+                    }
+                );
+            }
+            
+            if (lastName2 != null || name != null)
+            {
+                bookDto.AuthorPublicationList.Add(new AuthorPublicationDto
+                    {
+                        Author = new AuthorDto
+                        {
+                            FirstName = name,
+                            LastName = lastName2
+                        }
+                    }
+                );
+            }
+
+            _output.WriteLine($"Getting citation for {bookDto} in BOOK CITATION test.");
+            string citation = _citationService.GetCitation(bookDto);
             _output.WriteLine($"Got citation {citation} in BOOK CITATION test.");
 
             Assert.Equal(expected, citation);
